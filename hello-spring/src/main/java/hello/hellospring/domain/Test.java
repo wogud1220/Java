f266ea938dc909bb2674ef4565540fdfd46a8d37
윤재형 <yoonwogud@gmail.com>
2024-08-22 19:34:44 +0900

    Add requestParma

---
+++ hello-spring/src/main/java/hello/hellospring/domain/Test.java
@@ -0,0 +1,10 @@
+package hello.hellospring.domain;
+
+public class Test {
+
+    public static void main(String[] args) {
+        String s1 = new String("abcd");
+        String s2 = new String("abcd");
+        System.out.println(s1.equals(s2));
+    }
+}
---
+++ java-basic/src/construct/MemberDefault.java
@@ -0,0 +1,11 @@
+package construct;
+
+public class MemberDefault {
+    String name;
+
+    public MemberDefault(String name) {
+        System.out.println("MemberDefault.MemberDefault");
+        this.name = name;
+
+    }
+}
